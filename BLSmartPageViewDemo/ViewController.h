c49a5944711075fe412b6b4649e03ad66663856c
Landyu <landyu@192.168.10.105>
2015-08-07 17:22:09 +0800

    Initial Commit

---
+++ BLSmartPageViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BLSmartPageViewDemo
+//
+//  Created by Landyu on 15/8/7.
+//  Copyright (c) 2015年 Landyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BLSmartPageViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BLSmartPageViewDemo
+//
+//  Created by Landyu on 15/8/7.
+//  Copyright (c) 2015年 Landyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
